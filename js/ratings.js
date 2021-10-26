4903048636cb127e8fe877b82d736eae55275d98
Fardin Kibria <fardin_kibria@tjxcanada.ca>
2021-10-26 13:30:48 -0400

    project-demo-1

---
+++ js/ratings.js
@@ -0,0 +1,29 @@
+function collect_ratings(){
+    let ratings = {
+        count:0, 
+        sum: 0, 
+        average:0
+    };
+
+    let rating = 0;
+
+    const elements = document.querySelectorAll('rating');
+
+    elements.forEach(element => {
+        rating = parseInt(element.id.replace('star', ''));
+        ratings.count += parseInt(element.value);
+        ratings.sum += rating*parseInt(element.value);
+    })
+
+    if (ratings.count != 0){
+        ratings.average = sum/count;
+        return ratings;
+    }
+    
+    document.addEventListener('change', (event) => {
+        const ratings = collect_ratings();
+        ratings.average = toFixed(event.value.querySelector('#average'));
+    });
+    
+
+}
